1a69631d93c3bc18aa3e4711638ddab0ea024235
kasturi28 <kasturipansare2818@gmail.com>
2022-06-20 21:57:15 +0530

    checking employee is present or not

--- src/empwagecalculation/empwagecalculation.java
+++ src/empwagecalculation/empwagecalculation.java
@@ -6,6 +6,14 @@ public class empwagecalculation {
 
 		System.out.println("WELCOME TO EMPLOYEE WAGE CALCULATION PROGRAM...");
 		System.out.println("***********************************************");
+		
+		int isPresent = 1;
+		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+		if (empCheck == isPresent) {
+			System.out.println("Employee is present...");
+		} else {
+			System.out.println("Employee is absent...");
+		}
 
 	}
 
