7af4de07dff68ed25f485d430f2b89f2070b7ffb
kasturi28 <kasturipansare2818@gmail.com>
2022-06-20 22:03:43 +0530

    calculating employee totak wage

--- src/empwagecalculation/empwagecalculation.java
+++ src/empwagecalculation/empwagecalculation.java
@@ -6,11 +6,15 @@ public class empwagecalculation {
 
 		System.out.println("WELCOME TO EMPLOYEE WAGE CALCULATION PROGRAM...");
 		System.out.println("***********************************************");
-		
+
 		int isPresent = 1;
+		int wagePerHour = 20;
+		int workingHours = 8;
+		int employeeWage = wagePerHour * workingHours;
 		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
 		if (empCheck == isPresent) {
 			System.out.println("Employee is present...");
+			System.out.println("Employee wage = " + employeeWage);
 		} else {
 			System.out.println("Employee is absent...");
 		}
