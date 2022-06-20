f26da53c9eb57e664fca4a3b9a5febbe3c6c339b
kasturi28 <kasturipansare2818@gmail.com>
2022-06-20 21:44:11 +0530

    welcome message to employee

---
+++ src/empwagecalculation/empwagecalculation.java
@@ -0,0 +1,12 @@
+package empwagecalculation;
+
+public class empwagecalculation {
+
+	public static void main(String[] args) {
+
+		System.out.println("WELCOME TO EMPLOYEE WAGE CALCULATION PROGRAM...");
+		System.out.println("***********************************************");
+
+	}
+
+}
