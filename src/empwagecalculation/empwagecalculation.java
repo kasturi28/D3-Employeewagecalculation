1ee1eb8b5438cefcb08846774af5a4b6a548b2bb
kasturi28 <kasturipansare2818@gmail.com>
2022-06-21 21:17:14 +0530

    added part time

--- src/empwagecalculation/empwagecalculation.java
+++ src/empwagecalculation/empwagecalculation.java
@@ -7,18 +7,25 @@ public class empwagecalculation {
 		System.out.println("WELCOME TO EMPLOYEE WAGE CALCULATION PROGRAM...");
 		System.out.println("***********************************************");
 
-		int isPresent = 1;
 		int wagePerHour = 20;
-		int workingHours = 8;
-		int employeeWage = wagePerHour * workingHours;
-		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-		if (empCheck == isPresent) {
-			System.out.println("Employee is present...");
-			System.out.println("Employee wage = " + employeeWage);
+		int empFullTime = 1;
+		int empPartTime = 2;
+		int employeeWage = 0;
+		int workingHours = 0;
+
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		if (empCheck == empFullTime) {
+			System.out.println("Employee is present for full time...");
+			workingHours = 8;
+		} else if (empCheck == empPartTime) {
+			System.out.println("Employee is present for part time...");
+			workingHours = 4;
 		} else {
 			System.out.println("Employee is absent...");
+			workingHours = 0;
 		}
-
+		employeeWage = wagePerHour * workingHours;
+		System.out.println("employee wage = " + employeeWage);
 	}
 
 }
